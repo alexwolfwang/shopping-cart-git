da10889688f547c44acbc5b8027ec4253df924ed
alexwolfwang <alexwolfwang@gmail.com>
2017-01-26 22:03:35 -0500

    create and upload order model

---
+++ models/order.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Alex.W on 2017/1/26.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    user: {type: Schema.Types.ObjectId,ref: 'User'},
+    cart: {type: Object, required: true},
+    address: {type: String, required: true},
+    name: {type: String, required: true},
+    paymentId: {type: String, required: true}
+});
+
+module.exports = mongoose.model('Order',schema);
